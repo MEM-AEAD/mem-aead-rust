4c30c2f4efeff43e2e92b7d3b91e94e7a451d793
Philipp Jovanovic <philipp@jovanovic.io>
2016-07-28 11:37:52 +0200

    adding test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #[macro_use]
 extern crate mem_aead;
 
-//use std::num::Int;
-//use std::num::{Wrapping};
+use mem_aead::mro::{crypto_aead_encrypt,crypto_aead_decrypt};
+//use mem_aead::mro_kat;
 
-use mem_aead::{crypto_aead_encrypt,crypto_aead_decrypt};
-
-
+#[allow(dead_code)]
 fn print_state(x : &[u64; 16]) {
     println!("{:016X} {:016X} {:016X} {:016X}", x[ 0], x[ 1], x[ 2], x[ 3]);
     println!("{:016X} {:016X} {:016X} {:016X}", x[ 4], x[ 5], x[ 6], x[ 7]);
@@ -14,20 +12,24 @@ fn print_state(x : &[u64; 16]) {
     println!("{:016X} {:016X} {:016X} {:016X}", x[12], x[13], x[14], x[15]);
 }
 
+#[allow(dead_code)]
 fn print_bytes(v : &[u8]) {
     for i in 0..v.len() {
         print!("{:02X} ", v[i]);
-        if i % 16 == 15 {
+        if i % 8 == 7 {
             println!("");
         }
     }
-    println!("");
+    println!("\n");
 }
 
-const HLEN : usize = 256;
-const MLEN : usize = 257;
 
-fn main() {
+
+#[allow(dead_code)]
+fn test() {
+
+    const HLEN : usize = 256;
+    const MLEN : usize = 257;
 
     let k : &[u8; 32] = &[0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0xFF,0xEE,0xDD,0xCC,0xBB,0xAA,0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11,0x00];
     let n : &[u8; 16] = &[0xF0,0xE0,0xD0,0xC0,0xB0,0xA0,0x90,0x80,0x70,0x60,0x50,0x40,0x30,0x20,0x10,0x00];
@@ -48,38 +50,70 @@ fn main() {
     println!("PAYLOAD:");
     print_bytes(m);
 
-
     println!("========== ENCRYPTION ==========");
     crypto_aead_encrypt(c, h, m, n, k);
     println!("ENCRYPTED PAYLOAD + TAG:");
     print_bytes(c);
 
-    println!("========== ENCRYPTION ==========");
+    println!("========== DECRYPTION ==========");
     let result = crypto_aead_decrypt(m, h, c, n, k);
     println!("DECRYPTED PAYLOAD:");
     print_bytes(m);
 
     println!("{}", result);
 
-    // key
-    //let mut k = &mut[0u8; 32];
-    //store64_le(&mut k[0..], w);
-    //store64_le(&mut k[8..], x);
-    //store64_le(&mut k[16..], y);
-    //store64_le(&mut k[24..], z);
+}
+ 
 
-    // nonce
-    //let mut n = &mut[0u8; 16];
-    //store64_le(&mut n[0..], y);
-    //store64_le(&mut n[8..], z);
+#[allow(dead_code)]
+fn genkat() {
 
-    //print_bytes(v);
-    //let a = load64_le(&v[0..]);
-    //assert_eq!(a, z);
-    //println!("{:016X}", a);
-     
-    //mro_init_mask(mask, k, n);
-   
-    //print_state(mask);
+    const MAX_SIZE : usize = 768;
+
+    let k = &mut[0u8; 32];
+    let n = &mut[0u8; 16];
+    let m = &mut[0u8; MAX_SIZE];
+    let h = &mut[0u8; MAX_SIZE];
+    let c = &mut[0u8; MAX_SIZE + 32];
+
+    for i in 0..MAX_SIZE {
+        m[i] = (255 & (i * 197 + 123)) as u8;
+        h[i] = (255 & (i * 193 + 123)) as u8;
+    }
+    for i in 0..k.len() {
+        k[i] = (255 & (i * 191 + 123)) as u8;
+    }
+    for i in 0..n.len() {
+        n[i] = (255 & (i * 181 + 123)) as u8;
+    }
+
+    println!("extern crate mem_aead;");
+    println!("pub static KAT : &'static [u8] = &[ ");
+    for i in 0..MAX_SIZE {
+        
+        crypto_aead_encrypt(&mut c[0..i+32], &h[0..i], &m[0..i], n, k);
+
+        for j in 0..i+32 {
+            let s = |i,j| {
+                return if j+1==i+32 {""} else { return if j%8 ==7 {",\n"} else {", "} };
+            };
+            print!("0x{:02X}{}", c[j], s(i,j));
+        }
+
+        if i + 1 == MAX_SIZE {
+            println!("");
+        } else {
+            println!(",\n");
+        }
+    }
+    println!("];");
+}
+
+
+fn main() {
+
+    //genkat();
+      
+    //println!("{}", mro_kat::KAT[0]);
        
 }
---
+++ tests/mro_test.rs
@@ -0,0 +1,49 @@
+extern crate mem_aead;
+
+#[cfg(test)]
+mod test {
+
+use mem_aead::mro::{crypto_aead_encrypt,crypto_aead_decrypt};
+
+#[test]
+fn mro_test() {
+
+    const MAX_SIZE : usize = 768;
+
+    let k = &mut[0u8; 32];
+    let n = &mut[0u8; 16];
+    let m = &mut[0u8; MAX_SIZE];
+    let h = &mut[0u8; MAX_SIZE];
+    let c = &mut[0u8; MAX_SIZE + 32];
+
+    for i in 0..MAX_SIZE {
+        m[i] = (255 & (i * 197 + 123)) as u8;
+        h[i] = (255 & (i * 193 + 123)) as u8;
+    }
+    for i in 0..k.len() {
+        k[i] = (255 & (i * 191 + 123)) as u8;
+    }
+    for i in 0..n.len() {
+        n[i] = (255 & (i * 181 + 123)) as u8;
+    }
+
+    //let mut pos = 0;
+
+    for i in 0..MAX_SIZE {
+
+        crypto_aead_encrypt(&mut c[0..i+32], &h[0..i], &m[0..i], n, k);
+
+        //for j in 0..i+32 {
+        //    assert_eq!(KAT[pos + j ], c[j]);
+        //}
+
+        let w = &mut[0u8; MAX_SIZE];
+
+        assert!(crypto_aead_decrypt(&mut w[0..i], &h[0..i], &c[0..i+32], n, k));
+
+        //pos += i+32;
+
+    }
+}
+
+}
